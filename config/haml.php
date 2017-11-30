a62e8194036f5c25252eb69195368ffc130ef892
Vladislav <mrjordanvl@gmail.com>
2017-11-30 04:39:07 +0300

    ok

--- config/haml.php
+++ config/haml.php
@@ -4,12 +4,10 @@
 	/**
 	 * These options are passed directly to MtHaml's environment constructor
 	 */
-    'mthaml' => array(
-        'environment' => 'php',
-        'options' => array(
-            'enable_escaper' => false,
-        ),
-        'filters' => array(),
-    ),
+	'mthaml' => array(
+		'environment' => 'php',
+		'options' => array(),
+		'filters' => array(),
+	),
 
 );
